8bc4351e28a3838c8f7f8b25e1d8d59e8618f4fa
Mohd Rejoan <wearecrx@gmail.com>
2022-01-27 17:44:38 +0600

    project initialized

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ["./src/*.html", "./src/**/*.html"],
+    theme: {
+        extend: {
+            fontFamily: {
+                oswald: ["Oswald", "sans-serif"],
+                roboto: ["Roboto", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
